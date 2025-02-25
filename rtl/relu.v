3e960760ad712f50421816c15d8e32853d091da2
Adityav2003 <vatsadityavs@gmail.com>
2025-02-26 05:10:36 +0530

    successfully created relu rlt and tb

---
+++ rtl/relu.v
@@ -0,0 +1,19 @@
+module relu(
+	input clock,
+	input  [31:0] relu_in, //input for relu
+	input relu_valid, //valid flag to enable relu calculation
+	output reg [31:0] relu_out, //output for relu
+	output reg relu_done // flag to show that the calculation is done
+);
+
+	always @(posedge clock) begin
+		if(relu_valid) begin
+			if(relu_in[31] == 1'b1) relu_out <= 0;
+			else relu_out <= relu_in;
+
+			relu_done <= 1'b1;
+		end
+	end
+
+	
+endmodule
